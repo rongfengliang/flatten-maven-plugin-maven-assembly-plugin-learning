8ece18fadd6f23185d922f78cdad3fd2843a1666
rong fengliang <1141591465@qq.com>
2023-01-21 23:39:20 +0800

    add more

--- packages/src/main/java/com/dalong/App.java
+++ packages/src/main/java/com/dalong/App.java
@@ -1,7 +1,20 @@
 package com.dalong;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("demoapp");
+        Config config = ConfigFactory.load();
+        int age = config.getInt("service.com.dalong.app");
+        List<String> classpaths = config.getStringList("service.com.classpath");
+        String loginType = config.getString("service.userlogintype");
+        int newAge = config.getInt("dremio.myage");
+        System.out.println(age);
+        System.out.println(classpaths);
+        System.out.println(loginType);
+        System.out.println(newAge);
     }
 }
